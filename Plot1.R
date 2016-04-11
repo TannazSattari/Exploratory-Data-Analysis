524939c5c1c02b8be410ffe115b6adce0fb47620
Tannaz Sattari Tabrizi <t.sattari@gmail.com>
2016-04-10 23:32:05 -0600

    Plot1
    Create histogram and save it as a png.

--- Plot1.R
+++ Plot1.R
@@ -1,2 +1,18 @@
 #Read Data set
-data_full <- read.table(d"./data/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+filedir<-"C://Users//tsatt//OneDrive//Documents//R//household_power_consumption.txt"
+data_full <- read.table(filedir, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Subsetting
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+#Converting date
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+#Plotting and Saving file
+globalActivePower <- as.numeric(data$Global_active_power)
+hist(globalActivePower, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
